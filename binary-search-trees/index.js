8365d14eaf260426d43992d1e7a1402244111556
AnumMujahid <amujahid.bese18seecs@student.nust.edu.pk>
2022-07-21 22:46:48 +0500

    feat(): add tree  traversal methods to binary search tree class

--- binary-search-trees/index.js
+++ binary-search-trees/index.js
@@ -55,4 +55,51 @@ class BinarySearchTree {
     if (!found) return false;
     return current;
   }
+
+  bfs() {
+    let queue = [];
+    let visited = [];
+    let node = this.root;
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      visited.push(node);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return visited;
+  }
+
+  dfsPreOrder() {
+    let visited = [];
+    function traverse(node) {
+      visited.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
+  }
+
+  dfsPostOrder() {
+    let visited = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      visited.push(node.value);
+    }
+    traverse(this.root);
+    return visited;
+  }
+
+  dfsInOrder() {
+    let visited = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      visited.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
+  }
 }
